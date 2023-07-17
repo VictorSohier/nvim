423926ba10729c612faebf07b0a9c38292792df0
Victor Sohier <1sohiervic@gmail.com>
2023-07-16 21:57:21 -0400

    fix stupid remapping bug

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,20 +1,19 @@
 local lsp = require('lsp-zero').preset({})
 
 lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({buffer = bufnr})
+	lsp.default_keymaps({buffer = bufnr})
+	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end)
+	vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end)
+	vim.keymap.set("n", "gh", function() vim.lsp.buf.hover() end)
+	vim.keymap.set("n", "gH", function() vim.lsp.buf.code_action() end)
+	vim.keymap.set("n", "gr", function() vim.lsp.buf.references() end)
+	vim.keymap.set("n", "F2", function() vim.lsp.buf.rename() end)
 end)
 
 -- (Optional) Configure lua language server for neovim
 require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
 
 lsp.setup()
-
-vim.keymap.set("n", "<silent>gd", vim.lsp.buf.definition)
-vim.keymap.set("n", "<silent>gi", vim.lsp.buf.implementation)
-vim.keymap.set("n", "<silent>gh", vim.lsp.buf.hover)
-vim.keymap.set("n", "<silent>gH", vim.lsp.buf.code_action)
-vim.keymap.set("n", "<silent>gr", vim.lsp.buf.references)
-vim.keymap.set("n", "<silent>F2", vim.lsp.buf.rename)
 
 local cmp = require'cmp'
 cmp.setup {
